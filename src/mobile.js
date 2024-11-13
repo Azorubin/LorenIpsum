5f958ea17db0d54c59529cfa328b00a626fee526
Azorubin <karimovandr94@mail.ru>
2024-11-14 00:43:18 +0500

    Initial commit

---
+++ src/mobile.js
@@ -0,0 +1,43 @@
+import "./appMoblie.css";
+import "./globalMobile.css";
+document.getElementById("app").innerHTML = `
+<main>
+
+<div class="screen">
+      <div class="overlap-wrapper">
+        <div class="overlap">
+          <img class="img" src="src/img/0132932025ae287ae568d2b24ef4b8d1-1.png" />
+          <div class="div"></div>
+          <div class="h">Loren Ipsum</div>
+          <div class="view">
+            <div class="text-wrapper">Epc<br />Hunters</div>
+            <img class="stash-burger-classic" src="src/img/stash-burger-classic-light.svg" />
+          </div>
+          <div class="overlap-group-wrapper">
+            <div class="overlap-group">
+              <div class="image"></div>
+              <div class="view-2">
+                <div class="group"><img class="element" src="src/img/1.svg" /></div>
+                <p class="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              </div>
+              <img class="vector" src="src/img/vectorMobile.svg" />
+            </div>
+          </div>
+          <div class="slyder">
+            <div class="frame">
+              <img class="solar-arrow-up" src="src/img/arrowLeft.svg" />
+              <div class="text-wrapper-2">loren ipsum</div>
+            </div>
+            <div class="frame-2">
+              <div class="text-wrapper-3">loren ipsum</div>
+              <img class="solar-arrow-up" src="src/img/arrowRight.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+</main>
+
+`;
